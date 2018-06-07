2d16ffe649e0c5030154dd28f2ffd98aaa0daf21
lecocchi <leandro.cocchi@iunigo.com>
2018-06-07 16:56:23 -0300

    Add spinner in BacklogPage

--- src/pages/backlog/backlog.ts
+++ src/pages/backlog/backlog.ts
@@ -5,32 +5,35 @@ import {IssueProvider} from "../../providers/issue/issue";
 
 @IonicPage()
 @Component({
-    selector: 'page-backlog',
-    templateUrl: 'backlog.html',
+  selector: 'page-backlog',
+  templateUrl: 'backlog.html',
 })
 export class BacklogPage {
 
-    issues:any = [];
-    avatar:string = 'https://picsum.photos/300/300?image=0';
-    issuePage:any;
+  issues:any = [];
+  avatar:string = 'https://picsum.photos/300/300?image=0';
+  issuePage:any;
+  loading:boolean = true;
 
-    constructor(public navCtrl: NavController, public issueProvider: IssueProvider) {
-        this.issuePage = IssuePage;
-    }
+  constructor(public navCtrl: NavController, public issueProvider: IssueProvider) {
+    this.issuePage = IssuePage;
+  }
 
-    ionViewDidLoad(){
-        this.issueProvider.getAllIssueBacklog()
-            .subscribe(data =>{
-                this.issues = data;
-            });
-    }
+  ionViewDidLoad(){
+    this.issueProvider.getAllIssueBacklog()
+      .subscribe(data =>{
+        this.issues = data;
+        this.loading = false;
+      });
+  }
 
-    ionViewDidEnter(){
-        this.issueProvider.getAllIssueBacklog()
-            .subscribe(data =>{
-                this.issues = data;
-            });
-    }
+  ionViewDidEnter(){
+    this.issueProvider.getAllIssueBacklog()
+      .subscribe(data =>{
+        this.issues = data;
+        this.loading = false;
+      });
+  }
 
   openDetail(issue:any){
     this.navCtrl.push(IssuePage,{"issue":issue, "update":true, "backlog": true});
