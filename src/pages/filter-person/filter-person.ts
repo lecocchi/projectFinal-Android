cd52edd85b2c28b351792aac5d76a36202d093a6
lecocchi <leandro.cocchi@iunigo.com>
2018-06-29 09:33:05 -0300

    Add filter in Filter person page

--- src/pages/filter-person/filter-person.ts
+++ src/pages/filter-person/filter-person.ts
@@ -8,29 +8,45 @@ import {UserProvider} from "../../providers/user/user";
 })
 export class FilterPersonPage {
 
-    persons:any = [];
-    persona:string;
+  persons: any = [];
+  personToSearch:string;
+  personsToShow:any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-              public userProvider: UserProvider) {
-  }
+  // persons:any = [{
+  //   firstName: 'Leandro',
+  //   lastName: 'Cocchi'
+  // },
+  //   {
+  //     firstName: 'Julieta',
+  //     lastName: 'Meved'
+  //   },
+  //   {
+  //     firstName: 'Christian',
+  //     lastName: 'Cocchi'
+  //   },
+  //   {
+  //     firstName: 'Roberto',
+  //     lastName: 'Medved'
+  //   },
+  //   {
+  //     firstName: 'Graciela',
+  //     lastName: 'Duhay'
+  //   }];
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userProvider: UserProvider) { }
 
   ionViewDidLoad() {
     this.userProvider.getAllUser()
-        .subscribe( data => {
-            this.persons = data;
-        });
+      .subscribe( data => {
+        this.persons = data;
+        this.personsToShow = data;
+      });
   }
 
-    filterItems(){
-
-      //console.log(this.persona);
-        return this.persons.filter((item) => {
-            return item.firstName.toLowerCase().
-                indexOf(this.persona.toLowerCase()) > -1 ||
-                item.lastName.toLowerCase().
-                indexOf(this.persona.toLowerCase()) > -1;
-        });
-    }
+  filterItems(){
+    this.personsToShow = this.persons.filter(item =>
+      (item.firstName.toLowerCase().indexOf(this.personToSearch.toLowerCase()) >= 0   || item.lastName.toLowerCase().indexOf(this.personToSearch.toLowerCase()) >= 0 ));
+  }
 
 }
