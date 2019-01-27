578585593f25484abb342d2982e12de95b6f9191
lecocchi <leandro.cocchi@iunigo.com>
2019-01-27 12:00:47 -0300

    add new endpoint in config

--- src/components/config/config.ts
+++ src/components/config/config.ts
@@ -1,6 +1,7 @@
 // export const URL_BASE = "https://project-final-davinci.herokuapp.com";
 // export const URL_BASE = "http://leandro.hopto.org:8090";
 // export const URL_BASE = "http://localhost:8080";
-// export const URL_BASE = "http://localhost:8080";
-export const URL_BASE = "http://192.168.0.6:8080"
-// export const URL_BASE = "http://ec2-18-212-138-20.compute-1.amazonaws.com:8080";
+// export const URL_BASE = "http://localhost:8090";
+// export const URL_BASE = "http://192.168.0.6:8080"
+export const URL_BASE = "http://3.86.96.157:8090";
+// export const URL_BASE = "http://172.31.80.253:8090";
