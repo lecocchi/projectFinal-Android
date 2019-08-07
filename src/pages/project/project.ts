b3f88c02cb7436ac1a077f5ae47b2a8e15b4be12
MAC <lcocchi@droptek.com>
2019-08-07 13:44:04 -0300

    Add user in project

--- src/pages/project/project.ts
+++ src/pages/project/project.ts
@@ -11,23 +11,23 @@ import { DashboardProjectPage } from '../dashboard-project/dashboard-project';
   templateUrl: 'project.html',
 })
 export class ProjectPage {
-  members:any = [];
-  project:any;
-  id:number;
-  name:string = '';
-  description:string = '';
-  isCreate:boolean;
-  usersToAdd:any = [];
+  members: any = [];
+  project: any;
+  id: number;
+  name: string = '';
+  description: string = '';
+  isCreate: boolean;
+  usersToAdd: any = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public userProvider: UserProvider,
-              public loadingCtrl:LoadingController, 
-              public popoverCtrl: PopoverController,
-              public projectProvider: ProjectProvider,
-              public utilsProvider:UtilsProvider,
-              public alertCtrl: AlertController,
-              public utilProvider: UtilsProvider,
-              public storage: Storage) {
+    public userProvider: UserProvider,
+    public loadingCtrl: LoadingController,
+    public popoverCtrl: PopoverController,
+    public projectProvider: ProjectProvider,
+    public utilsProvider: UtilsProvider,
+    public alertCtrl: AlertController,
+    public utilProvider: UtilsProvider,
+    public storage: Storage) {
   }
 
   ionViewWillEnter() {
@@ -35,77 +35,78 @@ export class ProjectPage {
     this.project = this.navParams.get("project");
     this.isCreate = this.navParams.get("isCreate");
 
-    if(!this.isCreate){
+    if (!this.isCreate) {
 
       let loading = this.loadingCtrl.create(
-        { spinner: 'ios',
-          content:'Cargando...'
+        {
+          spinner: 'ios',
+          content: 'Cargando...'
         });
       loading.present();
 
       this.userProvider.getUserByProject(this.project.id)
-            .subscribe((users: any) => {
-              this.usersToAdd = users;
+        .subscribe((users: any) => {
+          this.usersToAdd = users;
 
-              for (let user of users) {
-                let personToShow: any = {
-                  "firstName": user.firstName,
-                  "lastName": user.lastName,
-                  "userName": user.userName,
-                  "avatar": user.avatar,
-                  "yesterday": "",
-                  "today": "",
-                  "checked": false
-                }
-                this.members.push(personToShow);
-              }
+          for (let user of users) {
+            let personToShow: any = {
+              "firstName": user.firstName,
+              "lastName": user.lastName,
+              "userName": user.userName,
+              "avatar": user.avatar,
+              "yesterday": "",
+              "today": "",
+              "checked": false
+            }
+            this.members.push(personToShow);
+          }
 
-                if(!this.isCreate){
-                  this.name = this.project.name;
-                  this.description = this.project.description;
-                }
+          if (!this.isCreate) {
+            this.name = this.project.name;
+            this.description = this.project.description;
+          }
 
-                loading.dismiss();
-        },
-        (err)=>{
           loading.dismiss();
-        });
-      }
+        },
+          (err) => {
+            loading.dismiss();
+          });
+    }
   }
 
-  cancel(){
+  cancel() {
     this.navCtrl.pop();
   }
 
-  createProject(){
+  createProject() {
 
     if (this.name == '') {
       this.utilsProvider.presentPrompt("Error", "El campo nombre no puede estar vacío");
 
     } else {
 
-      if(this.isCreate){
+      if (this.isCreate) {
         var project = {
           "name": this.name,
           "description": this.description
         }
 
         this.projectProvider.createProject(project)
-          .subscribe((p:any)=>{
-            
+          .subscribe((p: any) => {
+
             var projectUsers = {
               "project_id": p.id,
-              "users_id":this.usersToAdd
+              "users_id": this.usersToAdd
             }
 
             this.projectProvider.updateUsersInProject(projectUsers)
-              .subscribe(p1 =>{
+              .subscribe(p1 => {
                 this.navCtrl.push(DashboardProjectPage);
                 this.utilsProvider.presentToast(`Se creó el proyecto con éxito`);
               });
           });
 
-      }else{
+      } else {
 
         var projectUpdate = {
           "id": this.project.id,
@@ -113,15 +114,15 @@ export class ProjectPage {
           "description": this.description
         }
         this.projectProvider.updateProject(projectUpdate)
-          .subscribe((p:any)=>{
+          .subscribe((p: any) => {
 
             var projectUsers = {
-              "project_id":this.project.id,
-              "users_id":this.usersToAdd
+              "project_id": this.project.id,
+              "users_id": this.usersToAdd
             }
 
             this.projectProvider.updateUsersInProject(projectUsers)
-              .subscribe(p1 =>{
+              .subscribe(p1 => {
                 this.navCtrl.push(DashboardProjectPage);
                 this.utilsProvider.presentToast(`Se modificó el proyecto con éxito`);
               });
@@ -132,47 +133,76 @@ export class ProjectPage {
 
   }
 
-  addUsers(){
+  addUsers() {
 
     let loading = this.loadingCtrl.create(
-      { spinner: 'ios',
-        content:'Cargando...'
+      {
+        spinner: 'ios',
+        content: 'Cargando...'
       });
     loading.present();
 
     this.userProvider.getAllUser()
-      .subscribe((u:any) =>{
+      .subscribe((u: any) => {
 
-        if(this.isCreate){
+        if (this.isCreate) {
           loading.dismiss();
           let alert = this.alertCtrl.create();
           alert.setTitle('Usuarios');
 
+
           u.forEach(p => {
             alert.addInput({
-            type: 'checkbox',
-            label: p.firstName + " " + p.lastName,
-            value: p.id,
-            checked: false
+              type: 'checkbox',
+              label: p.firstName + " " + p.lastName,
+              value: p.id,
+              checked: this.members.some(pr => p.userName === pr.userName)
             });
           });
-          
+
+          // u.forEach(p => {
+          //   alert.addInput({
+          //     type: 'checkbox',
+          //     label: p.firstName + " " + p.lastName,
+          //     value: p.id,
+          //     checked: false
+          //   });
+          // });
+
           alert.addButton({
             text: 'Cancelar',
-            cssClass: 'btn-alert-cancel'});
+            cssClass: 'btn-alert-cancel'
+          });
 
-            alert.addButton({
-              text: 'Aceptar',
-              cssClass: 'btn-alert-ok',
-              handler: (data:any) => {
-                this.usersToAdd = data;
+          alert.addButton({
+            text: 'Aceptar',
+            cssClass: 'btn-alert-ok',
+            handler: (data: any) => {
+              this.usersToAdd = data;
+
+
+              this.members = [];
+
+              for (let d of data) {
+                for (let user of u) {
+                  if (user.id === d) {
+                    let personToShow: any = {
+                      "firstName": user.firstName,
+                      "lastName": user.lastName,
+                      "userName": user.userName,
+                      "checked": false
+                    }
+                    this.members.push(personToShow);
+                  }
+                }
               }
-            });
+            }
+          });
           alert.present();
-          
-        }else{
+
+        } else {
           this.userProvider.getUserByProject(this.project.id)
-            .subscribe((up:any)=>{
+            .subscribe((up: any) => {
 
               loading.dismiss();
 
@@ -181,31 +211,48 @@ export class ProjectPage {
 
               u.forEach(p => {
                 alert.addInput({
-                type: 'checkbox',
-                label: p.firstName + " " + p.lastName,
-                value: p.id,
-                checked: up.some(pr => p.id === pr.id)
+                  type: 'checkbox',
+                  label: p.firstName + " " + p.lastName,
+                  value: p.id,
+                  checked: this.members.some(pr => p.userName === pr.userName)
                 });
               });
-              
+
               alert.addButton({
                 text: 'Cancelar',
-                cssClass: 'btn-alert-cancel'});
+                cssClass: 'btn-alert-cancel'
+              });
 
-                alert.addButton({
-                  text: 'Aceptar',
-                  cssClass: 'btn-alert-ok',
-                  handler: (data:any) => {
-                    this.usersToAdd = data;
+              alert.addButton({
+                text: 'Aceptar',
+                cssClass: 'btn-alert-ok',
+                handler: (data: any) => {
+                  this.usersToAdd = data;
+
+                  this.members = [];
+
+                  for (let d of data) {
+                    for (let user of u) {
+                      if (user.id === d) {
+                        let personToShow: any = {
+                          "firstName": user.firstName,
+                          "lastName": user.lastName,
+                          "userName": user.userName,
+                          "checked": false
+                        }
+                        this.members.push(personToShow);
+                      }
+                    }
                   }
-                });
+                }
+              });
               alert.present();
             });
-          }
+        }
       });
   }
 
-  deleteProject(){
+  deleteProject() {
 
     const confirm = this.alertCtrl.create({
       title: 'Eliminar Proyecto',
@@ -220,17 +267,17 @@ export class ProjectPage {
           cssClass: 'btn-alert-ok',
           handler: () => {
             this.projectProvider.deleteProject(this.project.id)
-              .subscribe(()=>{
+              .subscribe(() => {
                 this.storage.get("projectId")
-                  .then(id=>{
+                  .then(id => {
                     this.navCtrl.push(DashboardProjectPage);
                     this.utilsProvider.presentToast(`Se elimino el proyecto con éxito`);
                   })
               },
-              (err)=>{
-                this.utilProvider.presentPrompt(err.error.title, err.error.message);
-              }
-            );
+                (err) => {
+                  this.utilProvider.presentPrompt(err.error.title, err.error.message);
+                }
+              );
           }
         }
       ]
